20718f8cf43567cdf1705a547cfc1f8316d8f36e
Jacobb26 <fmartinezd3@miumg.edu.gt>
2022-09-02 11:12:06 -0600

    ConteoFor

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System;
+
+internal class Program 
+{
+		static void Main(string[] args)  
+
+    {
+        {
+			int i;
+			i = 100;
+			for (i=100;i>=1;i--) {
+				Console.WriteLine(i);
+			}
+		}
+
+      }
+
+
+}
